48800b4e775bbab369316318af98b6f1b08db777
Shihao Xia <charlesxsh@hotmail.com>
2021-08-11 16:01:11 -0400

    remove chcov from testbins

--- fuzz-testbins.sh
+++ fuzz-testbins.sh
@@ -5,4 +5,4 @@ TEST_BINS_DIR=$1
 OUT_DIR=$2
 docker build -f testbins.Dockerfile -t gofuzz:latest .
 
-docker run -it -v $TEST_BINS_DIR:/fuzz/target -v $OUT_DIR:/fuzz/output  gofuzz:latest -testBinsDir /fuzz/target/testbins -chCover /fuzz/target/op-cov -outputDir /fuzz/output -parallel 5
+docker run -it -v $TEST_BINS_DIR:/fuzz/target -v $OUT_DIR:/fuzz/output  gofuzz:latest -testBinsDir /fuzz/target -outputDir /fuzz/output -parallel 5
