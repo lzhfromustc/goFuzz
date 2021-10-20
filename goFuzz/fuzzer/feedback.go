d9afa1c3a54156fe68f29c526c60d6c199b32ad2
Yu Liang <luy70@psu.edu>
2021-10-20 12:22:09 -0400

    Reverted back to the original Score function.

--- goFuzz/fuzzer/feedback.go
+++ goFuzz/fuzzer/feedback.go
@@ -2,7 +2,6 @@ package fuzzer
 
 import (
 	"log"
-	"math"
 )
 
 const (
@@ -21,87 +20,40 @@ const (
 //		ScorePeakBuffer: The score for each peak buffer would be: score = 10 * (PeakBuffer / BufferSize).
 func ComputeScore(mainRecord map[string]*Record, curRecord *Record, runResult *RunResult, id string, prevID string) int {
 	score := 0
-	var curTupleCount = 0
-	var preTupleCount = 0
-	var curTupleNum = 0
-	var preTupleNum = 0
-
 	var tupleCountScore = 0
-	var tupleNumScore = 0
-
-	if curRecord == nil {
-		return 0;
+	for _, count := range curRecord.MapTupleRecord {
+		score += int(count) * ScoreTupleCountLog2
+		tupleCountScore += int(count) * ScoreTupleCountLog2
 	}
 
-	for _, count := range runResult.RetRecord.MapTupleRecord {
-		curTupleCount += count
-		curTupleNum += 1
-		//log.Printf("Log: Get tuples count: " + strconv.Itoa(count))
+	var notClosedScore = 0
+	var bufferScore = 0
+	var chScore = 0
+
+	for _, chRecord := range curRecord.MapChanRecord {
+
+		// ScoreNewClosed/ScoreNotClosed: score if this is the first time for a closed/notclosed status of existing channel
+		if chRecord.NotClosed == true {
+			score += ScoreNotClosed
+			notClosedScore += ScoreNotClosed
+		}
+
+		// ScoreBuf: ScoreBuffer * BufferPercentage
+		if chRecord.PeakBuf > 0 && chRecord.CapBuf != 0 {
+			bufferPer := float64(chRecord.PeakBuf) / float64(chRecord.CapBuf)
+			score += int(float64(ScoreBuf) * bufferPer)
+			bufferScore += int(float64(ScoreBuf) * bufferPer)
+		}
+
+		// ScoreCh: score for each detected channel
+		score += ScoreCh
+		chScore += ScoreCh
 	}
-
-	//log.Printf("In computeScore, getting mainRecord size: " + strconv.Itoa(len(mainRecord)))
-
-	curMainRecord := mainRecord[prevID]
-
-	// If the previous main record is not nil. We can calculate the current record based on
-	// tuple differences. Otherwise, use score = 0
-	if curMainRecord != nil {
-		for _, count := range curMainRecord.MapTupleRecord {
-			countLog := math.Log2(float64(count))
-			if int(countLog) != -9223372036854775808 {
-				preTupleCount += int(countLog)
-				preTupleNum += 1
-			}
-		}
-
-		tupleCountScore = curTupleCount - preTupleCount
-		if tupleCountScore < 0 {
-			tupleCountScore = - tupleCountScore
-		}
-		tupleNumScore = curTupleNum - preTupleNum
-		if tupleNumScore < 0 {
-			tupleNumScore = - tupleNumScore
-		}
-	} else {
-		log.Printf("MainRecord is NULL. ")
-	}
-
-	//log.Printf("In current " + id + ", prevID:" + prevID + " curTupleCount: " + strconv.Itoa(curTupleCount) + "curTupleNum: " + strconv.Itoa(curTupleNum) + " preTupleCount: " + strconv.Itoa(preTupleCount) + " preTupleNum: " + strconv.Itoa(preTupleNum))
-
-	// Write curRecord, use it to save the current Record for the next run.
-	mainRecord[id] = curRecord
-
-	score = tupleCountScore + tupleNumScore
-
-	//var notClosedScore = 0
-	//var bufferScore = 0
-	//var chScore = 0
-	//
-	//for _, chRecord := range curRecord.MapChanRecord {
-	//
-	//	// ScoreNewClosed/ScoreNotClosed: score if this is the first time for a closed/notclosed status of existing channel
-	//	if chRecord.NotClosed == true {
-	//		score += ScoreNotClosed
-	//		notClosedScore += ScoreNotClosed
-	//	}
-	//
-	//	// ScoreBuf: ScoreBuffer * BufferPercentage
-	//	if chRecord.PeakBuf > 0 && chRecord.CapBuf != 0 {
-	//		bufferPer := chRecord.PeakBuf / chRecord.CapBuf
-	//		score += ScoreBuf * bufferPer
-	//		bufferScore += ScoreBuf * bufferPer
-	//	}
-	//
-	//	// ScoreCh: score for each detected channel
-	//	score += ScoreCh
-	//	chScore += ScoreCh
-	//}
 
 	log.Printf("Score_Log: For stdout case: %s, ScoreTupleCountLog2: %d", runResult.StdoutFilepath, tupleCountScore)
-	log.Printf("Score_Log: For stdout case: %s, ScoreTupleNum: %d", runResult.StdoutFilepath, tupleNumScore)
-	//log.Printf("Score_Log: For stdout case: %s, ScoreNotClosed: %d", runResult.StdoutFilepath, notClosedScore)
-	//log.Printf("Score_Log: For stdout case: %s, ScoreBuf: %d", runResult.StdoutFilepath, bufferScore)
-	//log.Printf("Score_Log: For stdout case: %s, ScoreCh: %d", runResult.StdoutFilepath, chScore)
+	log.Printf("Score_Log: For stdout case: %s, ScoreNotClosed: %d", runResult.StdoutFilepath, notClosedScore)
+	log.Printf("Score_Log: For stdout case: %s, ScoreBuf: %d", runResult.StdoutFilepath, bufferScore)
+	log.Printf("Score_Log: For stdout case: %s, ScoreCh: %d", runResult.StdoutFilepath, chScore)
 
 	return score
 }
