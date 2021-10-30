21efc8c1c00d92f24d591aa0c0c48e58c9a8a8df
Yu Liang <luy70@psu.edu>
2021-10-30 18:09:14 -0400

    Added more log for score calculation.

--- goFuzz/fuzzer/feedback.go
+++ goFuzz/fuzzer/feedback.go
@@ -22,13 +22,23 @@ func ComputeScore(mainRecord map[string]*Record, curRecord *Record, runResult *R
 	score := 0
 	var tupleCountScore = 0
 
+	var tupleCount = 0
+	var tupleNum = 0
+	var notclosedNum = 0
+	var peakBuffer = 0
+	var capBuffer = 0
+	var channelNum = 0
+
+
 	if curRecord == nil {
-		return 0;
+		return 0
 	}
 
 	for _, count := range curRecord.MapTupleRecord {
 		score += int(count) * ScoreTupleCountLog2
 		tupleCountScore += int(count) * ScoreTupleCountLog2
+		tupleCount += int(tupleCount)
+		tupleNum += 1
 	}
 
 	var notClosedScore = 0
@@ -41,6 +51,7 @@ func ComputeScore(mainRecord map[string]*Record, curRecord *Record, runResult *R
 		if chRecord.NotClosed == true {
 			score += ScoreNotClosed
 			notClosedScore += ScoreNotClosed
+			notclosedNum += 1
 		}
 
 		// ScoreBuf: ScoreBuffer * BufferPercentage
@@ -48,17 +59,23 @@ func ComputeScore(mainRecord map[string]*Record, curRecord *Record, runResult *R
 			bufferPer := float64(chRecord.PeakBuf) / float64(chRecord.CapBuf)
 			score += int(float64(ScoreBuf) * bufferPer)
 			bufferScore += int(float64(ScoreBuf) * bufferPer)
+			peakBuffer += chRecord.PeakBuf
+			capBuffer += chRecord.CapBuf
 		}
 
 		// ScoreCh: score for each detected channel
 		score += ScoreCh
 		chScore += ScoreCh
+		channelNum += 1
 	}
 
 	log.Printf("Score_Log: For stdout case: %s, ScoreTupleCountLog2: %d", runResult.StdoutFilepath, tupleCountScore)
 	log.Printf("Score_Log: For stdout case: %s, ScoreNotClosed: %d", runResult.StdoutFilepath, notClosedScore)
 	log.Printf("Score_Log: For stdout case: %s, ScoreBuf: %d", runResult.StdoutFilepath, bufferScore)
 	log.Printf("Score_Log: For stdout case: %s, ScoreCh: %d", runResult.StdoutFilepath, chScore)
+
+	log.Printf("SScore_LOG: For stdout case: %s, Tuple Count: %d, Tuple Num: %d, Not closed: %d, Buffer Size: %d, Buffer Max Size: %d, Channel Num: %d",
+		runResult.StdoutFilepath, tupleCount, tupleNum, notclosedNum, peakBuffer, capBuffer, channelNum )
 
 	return score
 }
